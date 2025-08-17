17585bbf9f188585c64c645acb5052277dc30806
Phillip Yan <phillip.m.yan@gmail.com>
2025-08-16 23:27:03 -0400

    temp

--- smartcontracts/launchpad/script/Deploy.s.sol
+++ smartcontracts/launchpad/script/Deploy.s.sol
@@ -2,14 +2,21 @@
 pragma solidity ^0.8.24;
 
 import "forge-std/Script.sol";
-import {Base} from "../src/base.sol"; // <-- import from base.sol
+import {PortfolioFactory} from "src/base.sol";
 
 contract Deploy is Script {
     function run() external {
         uint256 pk = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(pk);
 
-        Base base = new Base(); // <-- deploy your Base contract
+        // TODO: replace with real addresses on Base Sepolia
+        address usdc = 0x036CbD53842c5426634e7929541eC2318f3dCF7e;
+        address usdcFeed = 0xd30e2101a97dcbAeBCBC04F14C3f624E67A35165;
+        address wethFeed = 0x4aDC67696bA383F43DD60A9e78F2C97Fbbfc7cb1;
+        address router = 0x94cC0AaC535CCDB3C01d6787D6413C739ae12bc4;
+
+        PortfolioFactory factory = new PortfolioFactory(usdc, usdcFeed, wethFeed, router);
+        // Optionally set defaults here with setDefaultAssetConfig(...)
 
         vm.stopBroadcast();
     }
